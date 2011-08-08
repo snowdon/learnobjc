8da8a7047d4831767e736241eb45cdf9359fd79d
Steven Shen <shenxiaochun@corp.chinahr.com>
2011-08-08 16:43:01 +0800

    add file

---
+++ hello.m
@@ -0,0 +1,9 @@
+// First program example
+#import <Foundation/Foundation.h>
+int main (int argc, const char * argv[])
+{
+NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+NSLog (@"Programming is fun!");
+[pool drain];
+return 0;
+}
